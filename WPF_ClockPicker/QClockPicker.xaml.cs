85f9842a7a608d8bd89095592691e2f065dad4cd
QQ <oven425@yahoo.com.tw>
2018-02-24 00:12:31 +0800

    Add files via upload

---
+++ WPF_ClockPicker/QClockPicker.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ClockPicker
+{
+    /// <summary>
+    /// QClockPicker.xaml 的互動邏輯
+    /// </summary>
+    public partial class QClockPicker : UserControl
+    {
+        public QClockPicker()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this.grid.ActualHeight
+            
+        }
+    }
+}
